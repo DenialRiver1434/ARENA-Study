a3c43b432ba4172898aeb1f38214bfa9d18e6d47
Daniel <danielsge.main@gmail.com>
2026-09-08 23:40:52 -0400

    fix datasets merge

--- Chapter 0/pytorch-numpy-practice/datasets.py
+++ Chapter 0/pytorch-numpy-practice/datasets.py
@@ -1,11 +1,31 @@
-# Below is the code on docs.pytorch.org which
-# I'll annotate
+import torch
+from torchvision import datasets
+from torchvision.transforms import v2
+import matplotlib.pyplot as plt
+
+Fashion_training_data = datasets.FashionMNIST(
+    root="data", # Location
+    train=True, # Specifies this is for training
+    download=True, # Download from internet since not local
+    transform=v2.Compose([ # composes the following two transformations
+        v2.ToImage(), # converts to image type
+        v2.ToDtype(torch.float32, scale=True) # sets everything to 32-bit float
+    ])
+)
+
+Fashion_test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])
+)
 
 import os
 import pandas as pd
 from torchvision.io import decode_image
 from torch.utils.data import Dataset
 
+# CUSTOM DATASETS
 
 class CustomImageDataset(Dataset): 
     # ^ Auto-inherits all Dataset functions ^
