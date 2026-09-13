1f25e60933c27c2f195b2eceb7e06b79463f1d9b
Daniel <danielsge.main@gmail.com>
2026-09-12 23:07:07 -0400

    upload dataloaders

--- Chapter-0/pytorch-numpy-practice/dataloaders-transforms.py
+++ Chapter-0/pytorch-numpy-practice/dataloaders-transforms.py
@@ -1,6 +1,9 @@
 
 import torch
 from torchvision import datasets
+
+# Newer version of transforms
+# Always use (superset of transforms functions)
 from torchvision.transforms import v2
 
 training_data = datasets.FashionMNIST(root="data",train=True,download=True,transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]))
@@ -17,3 +20,11 @@ test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
 train_features, train_labels = next(iter(train_dataloader))
 
 print(f"Labels batch shape: {train_labels.size()}") # 64 labels
+
+# TRANSFORMS
+
+import torch.nn.functional as F
+
+target_transform = v2.Lambda(
+    lambda y: F.one_hot(torch.tensor(y), num_classes=10).float()
+)
